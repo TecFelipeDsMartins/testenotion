0f11ad58016ad2e64355e2b37014018fda4e1f6e
Felipe d s Martins <tec.felipedsmartins@gmail.com>
2023-03-07 18:41:53 -0300

    show data from api on front

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -10,5 +10,5 @@ export default async(req, res) => {
     database_id: process.env.NOTION_DATABASE_ID
   });
 
-  res.status(200).json({ response })
+  res.status(200).json({ results: response.results })
 }
